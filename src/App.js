11d18900007d5fec3564af35c4aab2f8fa87c74f
Your Name <you@example.com>
2023-03-24 20:04:50 +0300

    mini optimization

--- src/App.js
+++ src/App.js
@@ -1,80 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import BookCard from './BookCard';
 import Spinner from './Spinner';
 import './App.css';
-//массивы для сортировки и категории, устанвока шага пагинации
+
 const categories = ['all', 'art', 'biography', 'computers', 'history', 'medical', 'poetry'];
 const sortOptions = ['relevance', 'newest'];
 const STEP = 30;
 
 const App = () => {
-// состояния
-  const [books, setBooks] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('all');
-  const [selectedSortOption, setSelectedSortOption] = useState('relevance');
-  const [startIndex, setStartIndex] = useState(0);
-  const [totalItems, setTotalItems] = useState(0);
-  const [isSearching, setIsSearching] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+const [books, setBooks] = useState([]);
+const [searchQuery, setSearchQuery] = useState('');
+const [selectedCategory, setSelectedCategory] = useState('all');
+const [selectedSortOption, setSelectedSortOption] = useState('relevance');
+const [startIndex, setStartIndex] = useState(0);
+const [totalItems, setTotalItems] = useState(0);
+const [isSearching, setIsSearching] = useState(false);
+const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    // fetchBooks();
-  }, [searchQuery, selectedCategory, selectedSortOption, startIndex]);
-// получение данных, проброска и обновление состояния, принятие книг
-  const fetchBooks = async () => {
-    setIsSearching(true);
-    setIsLoading(true);
-    const startIndexParam = `startIndex=${startIndex}`;
-    const maxResultsParam = `maxResults=${STEP}`;
-    const orderByParam = `orderBy=${selectedSortOption}`;
-    const queryParam = searchQuery ? `q=${searchQuery}` : '';
-    const categoryParam = selectedCategory !== 'all' ? `subject:${selectedCategory}` : '';
-    const url = `https://www.googleapis.com/books/v1/volumes?${queryParam}&${categoryParam}&${orderByParam}&${startIndexParam}&${maxResultsParam}&key=AIzaSyCpMEW2Enid3P2K1niVagkq4qx4ByEmFCE`;
-    const response = await fetch(url);
-    const data = await response.json();
-    setBooks(data.items || []);
-    setTotalItems(data.totalItems || 0);
-    handleLoadMoreClick();
-    setIsSearching(false);
-    setIsLoading(false);
+const queryParams = useMemo(() => {
+const params = new URLSearchParams();
+params.set('startIndex', startIndex);
+params.set('maxResults', STEP);
+params.set('orderBy', selectedSortOption);
+if (searchQuery) {
+params.set('q', searchQuery);
+}
+if (selectedCategory !== 'all') {
+params.set('subject', selectedCategory);
+}
+return params;
+}, [searchQuery, selectedCategory, selectedSortOption, startIndex]);
 
-  };
-  // кнопка поиска
-  const handleSearchButtonClick = () => {
-    setStartIndex(0);
-    fetchBooks();
-  };
-  //кнопка выбора категории
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category);
-    setStartIndex(0);
-  };
-  //кнопка выбора сортировки
-  const handleSortOptionChange = (sortOption) => {
-    setSelectedSortOption(sortOption);
-    setStartIndex(0);
-  };
-  // функция для обновления состояния массива с book-list на слушатель load more
-  const handleLoadMoreClick = async () => {
-    setStartIndex(startIndex + STEP);
-    const key = "AIzaSyCpMEW2Enid3P2K1niVagkq4qx4ByEmFCE";
-    const startIndexParam = `startIndex=${startIndex + STEP}`;
-    const maxResultsParam = `maxResults=${STEP}`;
-    const orderByParam = `orderBy=${selectedSortOption}`;
-    const queryParam = searchQuery ? `q=${searchQuery}` : '';
-    const categoryParam = selectedCategory !== 'all' ? `subject:${selectedCategory}` : '';
-    const url = `https://www.googleapis.com/books/v1/volumes?${queryParam}&${categoryParam}&${orderByParam}&${startIndexParam}&${maxResultsParam}&key=${key}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const newBooks = data.items || [];
-    setBooks([...books, ...newBooks]);
-  };
+const fetchBooks = async () => {
+setIsSearching(true);
+setIsLoading(true);
+const url = `https://www.googleapis.com/books/v1/volumes?${queryParams.toString()}&key=AIzaSyCpMEW2Enid3P2K1niVagkq4qx4ByEmFCE`;
+const response = await fetch(url);
+const data = await response.json();
+setBooks(data.items || []);
+setTotalItems(data.totalItems || 0);
+handleLoadMoreClick();
+setIsSearching(false);
+setIsLoading(false);
+};
 
-  const handleSearchInputChange = (event) => {
-    setSearchQuery(event.target.value);
-    setStartIndex(0);
-  };
+useEffect(() => {
+// fetchBooks();
+}, [queryParams]);
+
+const handleSearchButtonClick = () => {
+setStartIndex(0);
+fetchBooks();
+};
+
+const handleCategoryChange = (category) => {
+setSelectedCategory(category);
+setStartIndex(0);
+};
+
+const handleSortOptionChange = (sortOption) => {
+setSelectedSortOption(sortOption);
+setStartIndex(0);
+};
+
+const handleLoadMoreClick = async () => {
+setStartIndex(startIndex + STEP);
+const url = `https://www.googleapis.com/books/v1/volumes?${queryParams.toString()}&key=AIzaSyCpMEW2Enid3P2K1niVagkq4qx4ByEmFCE`;
+const response = await fetch(url);
+const data = await response.json();
+const newBooks = data.items || [];
+setBooks([...books, ...newBooks]);
+};
+
+const handleSearchInputChange = (event) => {
+setSearchQuery(event.target.value);
+setStartIndex(0);
+};
+
 
 
 
